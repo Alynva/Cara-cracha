fd59b1c6460d2dabc5adeaf3fbfdf30809b6d6f5
rsaless <rspavarina@gmail.com>
2017-06-27 14:39:00 -0300

    classes pessoa e carteirinha
    criação das classes pessoa.h e carteirinha.h

---
+++ include/Carteirinha.h
@@ -0,0 +1,23 @@
+#ifindef
+#include iostream
+
+using namespace std;
+
+class Carteirinha {
+	public:
+		bool sexo;
+		int forma_do_rosto;
+		int cor_do_roso;
+		int oculos;
+		int cor_do_oculos;
+		int cor_da_pele;
+		int cabelo;
+		int cor_do_cabelo;
+		int chapeu;
+		int cor_do_chapeu;
+		int barba;
+		int cor_da_barba;
+		
+};
+
+#endif
---
+++ include/Pessoa.h
@@ -0,0 +1,30 @@
+#ifindef
+#include iostream
+
+using namespace std;
+
+class Pessoa {
+	public:
+		bool sexo;
+		int forma_do_rosto;
+		int cor_do_roso;
+		int oculos;
+		int cor_do_oculos;
+		int cor_da_pele;
+		int cabelo;
+		int cor_do_cabelo;
+		int chapeu;
+		int cor_do_chapeu;
+		int barba;
+		int cor_da_barba;
+		int blusa;
+		int cor_da_blusa;
+		int calca;
+		int cor_da_calca;
+		int tenis;
+		int cor_do_tenis;
+		carteirinha card;
+		
+};
+
+#endif
